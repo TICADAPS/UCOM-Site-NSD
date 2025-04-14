8c9809c5b9a14e593a5de197a93f8348f1948433
TICADAPS <marcelo.farias@adapsbrasil.com.br>
2025-04-14 11:02:40 -0300

    inclusão do rodapé

---
+++ includes/footer.php
@@ -0,0 +1,79 @@
+<div class="container-fluid" style="margin-bottom:0;background-color: #201f4e;">
+    <div class="row pb-5" style="padding: 0px 35px 10px 35px;">
+        <div class="col-md-3 mb-5 p-3">
+            <img class="img-fluid py-5" src="img/logo-adaps-text-white.png" alt="logo adaps" />
+            <h4 class="small text-white">Redes sociais</h4>
+            <a target="_blank" href="https://www.facebook.com/agenciasus">
+                <i class="fab fa-facebook text-white fa-2x mr-2 pl-2"></i>
+            </a>
+            <a target="_blank" href="https://www.instagram.com/agenciasus/">
+                <i class="fab fa-instagram text-white fa-2x mr-2"></i>
+            </a>
+            <a target="_blank" href="https://www.linkedin.com/company/84489833/admin/feed/posts/">
+                <i class="fab fa-linkedin text-white fa-2x mr-2"></i>
+            </a>
+            <a target="_blank" href="https://twitter.com/agenciasus">
+                <i class="fab fa-twitter text-white fa-2x mr-2"></i>
+            </a>
+            <a target="_blank" href="https://www.youtube.com/channel/UCLSEqv-F8oUfcHdyIgRhC9Q">
+                <i class="fab fa-youtube text-white fa-2x"></i>
+            </a>
+            <a target="_blank" href="https://www.flickr.com/photos/agenciasus">
+                <i class="fa-brands fa-flickr text-white fa-2x"></i>
+            </a>
+        </div>
+        <div class="col-md-3">
+            <p>
+            <h5 class="pt-5"><a class="small text-white" target="_blank" href="https://www.agenciasus.org.br/quem-somos/">Quem Somos</a></h5>
+            </p>
+            <p>
+            <h5><a class="small text-white" target="_blank" href="https://www.agenciasus.org.br/conselho/">Conselho</a></h5>
+            </p>
+            <p>
+            <h5><a class="small text-white" target="_blank" href="https://www.agenciasus.org.br/diretoria-executiva/">Diretoria Executiva</a></h5>
+            </p>
+            <p>
+            <h5><a class="small text-white" target="_blank" href="https://www.agenciasus.org.br/noticias/">Noticia</a></h5>
+            </p>
+            <p>
+            <h5><a class="small text-white" target="_blank" href="https://agenciasus.org.br/trabalheconosco/">Trabalhe Conosco</a></h5>
+            </p>
+        </div>
+        <div class="col-md-3">
+            <p>
+            <h5 class="pt-5"><a class="small text-white" href="https://www.agenciasus.org.br/transparencia-e-prestacao-de-contas/">Transparência</a></h5>
+            </p>
+            <p>
+            <h5><a class="small text-white" target="_blank" href="https://www.agenciasus.org.br/ouvidoria/">Ouvidoria</a></h5>
+            </p>
+            <p>
+            <h5><a class="small text-white" target="_blank" href="https://www.agenciasus.org.br/prestacao-de-contas/">Prestação de Contas</a></h5>
+            </p>
+            <p>
+            <h5><a class="small text-white" target="_blank" href="https://www.agenciasus.org.br/programa-medicos-pelo-brasil/">Programa Médicos pelo Brasil</a></h5>
+            </p>
+            <p>
+            <h5><a class="small text-white" target="_blank" href="https://agenciasus.org.br/contatos/">Contato</a></h5>
+            </p>
+        </div>
+        <div class="col-md-3">
+            <p><h4 class="small text-white pt-5">Sede Administrativa</h4></p>
+            <p class="small text-white"><i class="fas fa-phone text-white fa-2x"></i> (61) 3686-4144</p>
+            <p class="small text-white"><i class="fas fa-map-marker-alt text-white fa-2x"></i>
+                SHN – Quadra 1, Bloco E, Conj A, 2º andar, Asa Sul, Brasília – DF -CEP: 70.701-050
+            </p><br>
+            <p><h4 class="small text-white">Escritório Regional de Boa Vista</h4></p>
+            <p class="small text-white"><i class="fas fa-map-marker-alt text-white fa-2x"></i>
+                Rua Castelo Branco, 452 – Calunga <br> Boa Vista – RR <br> CEP: 69301-270
+            </p>
+        </div>
+    </div>
+</div>
+
+<script>
+    $(document).ready(function() {
+        $('[data-toggle="popover"]').popover();
+    });
+</script>
+</body>
+</html>
